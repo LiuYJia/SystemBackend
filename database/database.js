9cf8f6db182d0c0903f25b129e7b5b2edb2196f7
LiuYJia <1158502533@qq.com>
2020-04-13 10:14:35 +0800

    databaseconfig

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 var pool  = mysql.createPool({
     host     : '127.0.0.1',
     user     : 'root',
-    password : 'jiajia1996930',
+    password : '12345',
     database : 'system_backend'
 });
 
